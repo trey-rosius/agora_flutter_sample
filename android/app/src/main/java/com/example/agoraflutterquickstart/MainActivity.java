b35817c40ccd4761853f11de1ef4a672fc76be6d
iMAC <treyrosius@gmail.com>
2021-01-19 21:30:10 +0100

    first commit

---
+++ android/app/src/main/java/com/example/agoraflutterquickstart/MainActivity.java
@@ -0,0 +1,8 @@
+package com.example.agoraflutterquickstart;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+
+
+}
---
+++ lib/src/utils/settings.dart
@@ -0,0 +1,3 @@
+/// Define App ID and Token
+const APP_ID = "c3f0c2a1f1da4516ab6d6d7895acef5d";
+const Token = "006c3f0c2a1f1da4516ab6d6d7895acef5dIAAIhV/QPTn0nwceIQjJg+3RA8Q8rvhAzS+SPQKrJ8jeWEVWUPwAAAAAEAAlICC+J04HYAEAAQAnTgdg";
